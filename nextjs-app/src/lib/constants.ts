6e2370d1c5bdbbe9b61659bda2978a731a2c7696
im-hamza-dev <hi.hamza.dev@gmail.com>
2026-02-28 02:11:17 +0500

    fix const

--- nextjs-app/src/lib/constants.ts
+++ nextjs-app/src/lib/constants.ts
@@ -1,5 +1,5 @@
 // deployed
-export const SIGNAL_URL = process.env.NEXT_PUBLIC_SIGNAL_URL || 'clonecam-production.up.railway.app'  //'http://192.168.18.249:3001';
+export const SIGNAL_URL = process.env.NEXT_PUBLIC_SIGNAL_URL || 'https://clonecam-production.up.railway.app'  //'http://192.168.18.249:3001';
 
 // ngrok url
 // export const SIGNAL_URL= 'https://unabased-asha-nonrecurently.ngrok-free.dev/'
